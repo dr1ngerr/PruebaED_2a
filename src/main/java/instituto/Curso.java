245aa7dc65a000d7614e8c79e62b1b83f4a251d4
dr1ngerr <benedecandrei24@gmail.com>
2024-04-08 19:50:54 +0200

    Documentadas las clases

--- src/main/java/instituto/Curso.java
+++ src/main/java/instituto/Curso.java
@@ -6,23 +6,36 @@ package instituto;
 
 import java.util.TreeSet;
 /**
- *
+ * Clase que representa un curso en el que se matriculan personas
  * @author andreib
+ * @version 1.0
  */
 public class Curso {
 
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
+    /**
+     * Metodo getter para obtener el nombre del curso
+     * @return El nombre del curso
+     */
     protected String getNombre() {
         return nombre;
     }
 
+    /**
+     * Constructor que permite crear los cursos
+     * @param nombre Recibe el nombre que se le va a dar al curso
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Metodo que muestra una representacion en cadena del objeto Curso
+     * @return 
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -34,6 +47,10 @@ public class Curso {
         return s;
     }
 
+    /**
+     * Metodo para añadir alumnos al curso
+     * @param p El alumno que se quiere añadir
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
